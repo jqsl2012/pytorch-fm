f4ff0f6cdaa43af8b55faf029a679e8ee94e95ad
huangfei <huangfei>
2020-09-06 23:37:32 +0800

    .

--- examples/run.sh
+++ examples/run.sh
@@ -1,2 +1,5 @@
 
-python main.py --model_name lr --dataset_path /mnt/disk1/best_flow/git/pytorch-fm/examples/criteo/train.txt --device cpu --save_dir /mnt/disk1/best_flow/git/pytorch-fm/examples/save_dir --batch_size 12
+#python main_params.py --model_name lr --dataset_path /mnt/disk1/best_flow/git/pytorch-fm/examples/criteo/train.txt --device cpu --save_dir /mnt/disk1/best_flow/git/pytorch-fm/examples/save_dir --batch_size 12
+
+rm -rf .criteo/
+python main_params.py --model_name lr --dataset_path /mnt/disk1/best_flow/git/pytorch-fm/examples/train.txt --device cpu --save_dir /mnt/disk1/best_flow/git/pytorch-fm/examples/save_dir --batch_size 1024
