d38108976b4525214110a37dd7e7d49bd94becdb
huangjingjing <huangjingjing@tanzhou.cn>
2020-09-09 18:10:59 +0800

    no message

--- examples/main_params.py
+++ examples/main_params.py
@@ -47,7 +47,8 @@ def get_model(name, dataset):
     elif name == 'fm':
         return FactorizationMachineModel(field_dims, embed_dim=16)
     elif name == 'hofm':
-        return HighOrderFactorizationMachineModel(field_dims, order=3, embed_dim=16)
+        # return HighOrderFactorizationMachineModel(field_dims, order=3, embed_dim=16)
+        return LogisticRegressionModel(field_dims)
     elif name == 'ffm':
         return FieldAwareFactorizationMachineModel(field_dims, embed_dim=4)
     elif name == 'fnn':
