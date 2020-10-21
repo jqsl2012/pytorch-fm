364a57b23e95b6fb1c9331651a2164623d2c4c6e
huangjingjing <huangjingjing@tanzhou.cn>
2020-10-21 18:21:35 +0800

    no message

--- examples/prod_run.sh
+++ examples/prod_run.sh
@@ -1,3 +1,8 @@
 
-nohup python -u  main_params.py --model_name ipnn --dataset_path /home/eduapp/best_flow/20200907_more2more/all_features.train.fe_output.csv --device cpu --save_dir /home/eduapp/pytorch-fm/examples_prod/ --batch_size 2048 --epoch 2000   > run2.log.ipnn 2>&1 &
-tail -f run2.log.ipnn
+#nohup python -u  main_params.py --model_name ipnn --dataset_path /home/eduapp/best_flow/20200907_more2more/all_features.train.fe_output.csv --device cpu --save_dir /home/eduapp/pytorch-fm/examples_prod/ --batch_size 2048 --epoch 2000   > run2.log.ipnn 2>&1 &
+#tail -f run2.log.ipnn
+
+# for best_flow: release/release-1.1.0
+nohup python -u main_params.py --model_name opnn --dataset_path /home/eduapp/best_flow/20200907_more2more/a_9month_train.csv --device cpu --save_dir /home/eduapp/pytorch-fm/examples/model/ --batch_size 2048 --epoch 20   > run2.log.opnn 2>&1 &
+
+tail -f run2.log.opnn
